5c38f61128eaaec86e62b543cdbdb149a24b7bb6
MagdalenaMie <madeladesign@gmail.com>
2024-04-30 10:54:33 +0300

    added zmiana tla lvl3

--- UseStagelvl3/src/App.jsx
+++ UseStagelvl3/src/App.jsx
@@ -21,14 +21,39 @@ function App() {
        )
       }
 
+      // zmiana tla lvl3
+      const squares = [1, 2, 3, 4, 5]
+
+      const [selectedSquares, setSelectedSquares] =useState([])
+
+      const handleToggleBg = (id) => {
+        setSelectedSquares(prevSelectedSquares => {
+          if(prevSelectedSquares.includes(id)) {
+            return prevSelectedSquares.filter(selectedSquare => selectedSquare !== id)
+          } else {
+            return [...prevSelectedSquares, id]
+          }
+        })
+      }
+
   return (
     <>
-      {/* zmiana tla */}
+      {/* zmiana tla lvl3 */}
+      <div className="div-wrap">
+        {squares.map(square =>
+        <DivElement 
+        key={square}
+        onClick={()=>{handleToggleBg(square)}}
+        style={{backgroundColor: selectedSquares.includes(square) ? 'white' : 'transparent'}}
+        />)}
+      </div>
+
+
+      {/* zmiana tla lvl2*/}
       <div className="div-wrap">
       {buttons.map(button => 
         <DivElement 
         key={button}
-        content={''}
         onClick={() => handleSetChangeBg(button)}
         style={{ backgroundColor: changeBg===button ? 'white' : 'transparent' }}
         />
